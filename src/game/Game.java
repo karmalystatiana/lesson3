19ced337f8b306c4de877c88af720785a20a166a
Tanya <karmalyst@gmail.com>
2022-02-25 18:21:21 +0300

    камень

---
+++ src/game/Game.java
@@ -0,0 +1,18 @@
+package game;
+
+import java.util.Scanner;
+
+public class Game {
+
+    public static void main(String[] args) {
+	int a = 0;
+    while ( a = 5 ) {
+        int камень = 1;
+        int бумага = 2;
+        int ножницы = 3;
+        System.out.println( "Играем в камень, ножницы, бумага. Если вы хотите закончить игру введите 0./n Введите значение:" );
+        Scanner scanner = new Scanner(System.in);
+        int b = Integer.parseInt(scanner.next());
+    }
+    }
+}
