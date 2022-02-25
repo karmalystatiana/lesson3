74808e9173eedc27fb8ff8c175deb6d58f989fd4
Tanya <karmalyst@gmail.com>
2022-02-25 19:41:54 +0300

    Add tack_1

---
+++ src/game/task_1.java
@@ -0,0 +1,35 @@
+package game;
+
+import java.util.Scanner;
+
+public class task_1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите номер месяца");
+        int numMonth = scanner.nextInt();
+                switch (numMonth) {
+                    case 12:
+                    case 1:
+                    case 2:
+                        System.out.println("Зима");
+                        break;
+                    case 3:
+                    case 4:
+                    case 5:
+                        System.out.println("Весна");
+                        break;
+                    case 6:
+                    case 7:
+                    case 8:
+                        System.out.println("Лето");
+                    case 9:
+                    case 10:
+                    case 11:
+                        System.out.println("Осень");
+                        break;
+                    default:
+                        break;
+                }
+    }
+}
