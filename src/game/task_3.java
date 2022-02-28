29111e2cfb9a6215250945828d42b7b5cf247468
Tanya <karmalyst@gmail.com>
2022-02-28 19:05:25 +0300

    Add tack_3

--- src/game/task_3.java
+++ src/game/task_3.java
@@ -1,8 +1,21 @@
 package game;
 
+import java.util.Scanner;
+
 public class task_3 {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число");
+        int numMonth = scanner.nextInt();
+        int a = numMonth % 2;
+
+        if(a > 0) {
+            System.out.println("нечетное");
+        } else{
+            System.out.println("четное");
+        }
 
     }
 }
